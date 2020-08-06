720cb3e845f020b10a7c7631ebb2850f39b2af0f
black-desk <814727823@qq.com>
2020-08-06 20:14:22 +0800

    update
    add st.vbs to start st from windows
    
    remove wallpaper auto refresh script for ubuntu

--- refreshwallpaper.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-
-export GIO_EXTRA_MODULES=/usr/lib/x86_64-linux-gnu/gio/modules/
-wget -O $my_tools_dir/wallpaper.jpg https://unsplash.it/1920/1080/?random
-gsettings set org.gnome.desktop.background picture-uri file:///$my_tools_dir/wallpaper.jpg
